64efcb47283649263968ad99d71b05a1f72a6b02
Ben Lieber <blieber@comverge.com>
2014-03-26 12:43:03 -0600

    Allow for two range sensors Start parsing GPS packets to tinyGPS (not ustilizing yet)

--- ProjectPbr.ino
+++ ProjectPbr.ino
@@ -3,9 +3,20 @@
 #include "SoftwareSerial.h"
 #include "TinyGPS++.h"
 
-int const GREEN = 2;
-int const RED = 3;
-int const YELLOW = 4;
+// Not configurable:
+// GPS_RX = 18
+// GPS_TX = 19
+
+int const GREEN_LEFT = 2;
+int const RED_LEFT = 3;
+int const YELLOW_LEFT = 4;
+int const GREEN_RIGHT = 8;
+int const RED_RIGHT = 9;
+int const YELLOW_RIGHT = 10;
+
+int const RANGE_LEFT = 7;
+int const RANGE_RIGHT = 6;
+
 int const STOP_DIST_CM = 25;
 int const SLOW_DIST_CM = 50;
 int const READ_FREQ_MS = 300;
@@ -50,11 +61,18 @@ long Ultrasonic::microsecondsToCentimeters(void) {
 	return duration/29/2;
 }
 
-Ultrasonic ultrasonic(7);
+Ultrasonic ultrasonic_left(RANGE_LEFT);
+Ultrasonic ultrasonic_right(RANGE_RIGHT);
+
 void setup() {
-	pinMode(GREEN, OUTPUT);
-	pinMode(RED, OUTPUT);
-	pinMode(YELLOW, OUTPUT);
+	pinMode(GREEN_LEFT, OUTPUT);
+	pinMode(RED_LEFT, OUTPUT);
+	pinMode(YELLOW_LEFT, OUTPUT);
+
+  pinMode(GREEN_RIGHT, OUTPUT);
+  pinMode(RED_RIGHT, OUTPUT);
+  pinMode(YELLOW_RIGHT, OUTPUT);
+
 	Serial.begin(9600);
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);    // Set the update rate: 1 Hz
@@ -79,49 +97,73 @@ void useInterrupt(boolean v) {
 uint32_t timer = millis();
 
 void loop() {
-	if (GPS.newNMEAreceived()) {
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+
+  if (GPS.newNMEAreceived()) {
+
+    char *lastNMEA = GPS.lastNMEA();
+
+    int len = strlen( lastNMEA );
+    for (int i = 0; i < len; i++){
+      gps.encode(lastNMEA[i]);
+    }
+
+    if (!GPS.parse(lastNMEA))   // this also sets the newNMEAreceived() flag to false
       return;  // we can fail to parse a sentence in which case we should just wait for another
   }
 
   // if millis() or timer wraps around, we'll just reset it
   if (timer > millis())  timer = millis();
-  if (millis() - timer > 1000) {
+
+  if (millis() - timer > READ_FREQ_MS) {
 		if (GPS.fix) {
 			printGpsInfo();
 		} else {
 		  Serial.print("No Fix");
 		}
+
+    ultrasonic_right.DistanceMeasure(); // get the current signal time;
+    ultrasonic_left.DistanceMeasure(); // get the current signal time;
+
+    printRangeInfo(ultrasonic_left.microsecondsToCentimeters(), ultrasonic_right.microsecondsToCentimeters());
+    Serial.println();
 	}
-
-	long RangeInCentimeters;
-	ultrasonic.DistanceMeasure(); // get the current signal time;
-	RangeInCentimeters = ultrasonic.microsecondsToCentimeters();
-	printRangeInfo(RangeInCentimeters);
-	Serial.println();
-
-	delay(READ_FREQ_MS);
 }
 
-void updateSpeedIndicators(int pin) {
-	analogWrite(GREEN, 0);
-	analogWrite(YELLOW, 0);
-	analogWrite(RED, 0);
-	analogWrite(pin, 50);
+void updateSpeedIndicators(int pin1, int pin2) {
+  analogWrite(GREEN_LEFT, 0);
+  analogWrite(YELLOW_LEFT, 0);
+  analogWrite(RED_LEFT, 0);
+  analogWrite(GREEN_RIGHT, 0);
+  analogWrite(YELLOW_RIGHT, 0);
+  analogWrite(RED_RIGHT, 0);
+  analogWrite(pin1, 50);
+  analogWrite(pin2, 50);
 }
 
-void printRangeInfo(int rangeCm) {
+void printRangeInfo(long rangeCmL, long rangeCmR) {
 	Serial.print(", ");
-	Serial.print(rangeCm);
+	Serial.print(rangeCmL);
 	Serial.print("cm, ");
-	if (rangeCm < STOP_DIST_CM) {
-		updateSpeedIndicators(RED);
-		Serial.print("STOP!");
-	} else if (rangeCm < SLOW_DIST_CM) {
-		updateSpeedIndicators(YELLOW);
-		Serial.print("SLOW...");
+	if (rangeCmL < STOP_DIST_CM && rangeCmR < STOP_DIST_CM) {
+    updateSpeedIndicators(RED_LEFT, RED_RIGHT);
+    Serial.print("Reverse!");
+  } else if (rangeCmR < STOP_DIST_CM && rangeCmL > STOP_DIST_CM) {
+    updateSpeedIndicators(GREEN_LEFT, RED_RIGHT);
+    Serial.print("Hard left");
+  } else if (rangeCmL < STOP_DIST_CM && rangeCmR > STOP_DIST_CM) {
+		updateSpeedIndicators(RED_LEFT, GREEN_RIGHT);
+		Serial.print("Hard right");
+  } else if (rangeCmL < SLOW_DIST_CM && rangeCmR < SLOW_DIST_CM) {
+    updateSpeedIndicators(YELLOW_LEFT, YELLOW_RIGHT);
+    Serial.print("Slow down");
+  } else if (rangeCmR < SLOW_DIST_CM && rangeCmL > SLOW_DIST_CM) {
+    updateSpeedIndicators(GREEN_LEFT, YELLOW_RIGHT);
+    Serial.print("Soft left");
+  } else if (rangeCmL < SLOW_DIST_CM && rangeCmR > SLOW_DIST_CM) {
+    updateSpeedIndicators(YELLOW_LEFT, GREEN_RIGHT);
+    Serial.print("Soft right");
 	} else {
-		updateSpeedIndicators(GREEN);
+		updateSpeedIndicators(GREEN_LEFT, GREEN_RIGHT);
 		Serial.print("BALLS TO THE WALL!");
 	}
 }
