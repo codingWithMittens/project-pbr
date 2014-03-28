760839d4f58cab274269af8b7663a39eed56e968
Ben Lieber <blieber@comverge.com>
2014-03-27 18:41:57 -0600

    wip - doesn't output anything

--- ProjectPbr.ino
+++ ProjectPbr.ino
@@ -1,31 +1,31 @@
-#include "Arduino.h"
-#include "Adafruit_GPS.h"
-#include "SoftwareSerial.h"
-#include "TinyGPS++.h"
+#include <Arduino.h>
+// #include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 // pins:
 int const GREEN_LEFT   = 2,  RED_LEFT     = 3,  YELLOW_LEFT = 4;
 int const GREEN_RIGHT  = 8,  RED_RIGHT    = 9,  YELLOW_RIGHT = 10;
 int const RANGE_LEFT   = 7,  RANGE_RIGHT  = 6;
+int const RX = 18, TX = 19;
 
 // configuration:
 int const STOP_DIST_CM = 25, SLOW_DIST_CM = 50, READ_FREQ_MS = 300;
 
-// Not configurable:
-// GPS_RX = 18, GPS_TX = 19
-
-Adafruit_GPS GPS(&Serial1);
+// Adafruit_GPS GPS(&Serial1);
 // #define GPSECHO  false // debug?
 
 TinyGPSPlus gps;
+SoftwareSerial ss(RX, TX);
 
 class Ultrasonic {
 	public:
-		Ultrasonic(int pin);
+    Ultrasonic(int pin);
     void DistanceMeasure(void);
-		long microsecondsToCentimeters(void);
-	private:
-		int _pin;
+    long microsecondsToCentimeters(void);
+  private:
+    int _pin;
     long duration;
 };
 
@@ -36,13 +36,13 @@ Ultrasonic::Ultrasonic(int pin) {
 // Begin the detection and get the pulse back signal
 void Ultrasonic::DistanceMeasure(void) {
   pinMode(_pin, OUTPUT);
-	digitalWrite(_pin, LOW);
-	delayMicroseconds(2);
-	digitalWrite(_pin, HIGH);
-	delayMicroseconds(5);
-	digitalWrite(_pin,LOW);
-	pinMode(_pin,INPUT);
-	duration = pulseIn(_pin,HIGH);
+  digitalWrite(_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(_pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(_pin,LOW);
+  pinMode(_pin,INPUT);
+  duration = pulseIn(_pin,HIGH);
 }
 
 // The measured distance from the range 0 to 400 Centimeters
@@ -62,16 +62,16 @@ void setup() {
   pinMode(RED_RIGHT, OUTPUT);
   pinMode(YELLOW_RIGHT, OUTPUT);
 
-	Serial.begin(115200);
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // Set the update rate: 1 Hz
+  Serial.begin(115200);
+  ss.begin(9600);
+  // ss.write(PMTK_SET_NMEA_UPDATE_1HZ); // Set the update rate: 1 Hz
   useInterrupt();
   delay(1000);
 }
 
 // Interrupt is called once a millisecond, looks for any new GPS data, and feeds it to the encoder
 SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
+  char c = ss.read();
   gps.encode(c);
 }
 
@@ -80,20 +80,22 @@ void useInterrupt() {
   TIMSK0 |= _BV(OCIE0A);
 }
 
-uint32_t timer = millis();
+// uint32_t timer = millis();
 
 void loop() {
 	if (gps.location.isUpdated()) {
     Serial.print("Passed Checksum:");
     Serial.print(!gps.failedChecksum());
     Serial.print("; ");
-		printGpsInfo();
-    ultrasonic_right.DistanceMeasure(); // get the current signal time;
-    ultrasonic_left.DistanceMeasure(); // get the current signal time;
+    printGpsInfo();
+
+    // get the current signal time;
+    ultrasonic_right.DistanceMeasure();
+    ultrasonic_left.DistanceMeasure();
 
     printRangeInfo(ultrasonic_left.microsecondsToCentimeters(), ultrasonic_right.microsecondsToCentimeters());
     Serial.println();
-	}
+  }
 }
 
 void updateSpeedIndicators(int pin1, int pin2) {
@@ -121,8 +123,8 @@ void printRangeInfo(long rangeCmL, long rangeCmR) {
     updateSpeedIndicators(GREEN_LEFT, RED_RIGHT);
     Serial.print("Hard left");
   } else if (rangeCmL < STOP_DIST_CM && rangeCmR > STOP_DIST_CM) {
-		updateSpeedIndicators(RED_LEFT, GREEN_RIGHT);
-		Serial.print("Hard right");
+    updateSpeedIndicators(RED_LEFT, GREEN_RIGHT);
+    Serial.print("Hard right");
   } else if (rangeCmL < SLOW_DIST_CM && rangeCmR < SLOW_DIST_CM) {
     updateSpeedIndicators(YELLOW_LEFT, YELLOW_RIGHT);
     Serial.print("Slow down");
@@ -132,10 +134,10 @@ void printRangeInfo(long rangeCmL, long rangeCmR) {
   } else if (rangeCmL < SLOW_DIST_CM && rangeCmR > SLOW_DIST_CM) {
     updateSpeedIndicators(YELLOW_LEFT, GREEN_RIGHT);
     Serial.print("Soft right");
-	} else {
-		updateSpeedIndicators(GREEN_LEFT, GREEN_RIGHT);
-		Serial.print("BALLS TO THE WALL!");
-	}
+  } else {
+    updateSpeedIndicators(GREEN_LEFT, GREEN_RIGHT);
+    Serial.print("BALLS TO THE WALL!");
+  }
 }
 
 void printGpsInfo() {
@@ -149,8 +151,8 @@ void printGpsInfo() {
   // Serial.println(gps.location.rawLngDegrees()); // Raw longitude in whole degrees (no work!)
   // Serial.println(gps.location.rawLngBillionths()); // ... and billionths (i16/u32) (no work!)
   Serial.print("(within ");
-  Serial.print(gps.hdop.value());
-  Serial.print("); ");
+    Serial.print(gps.hdop.value());
+    Serial.print("); ");
   // Serial.println(gps.time.value()); // Raw time in HHMMSSCC format (u32) (keeping for last read calcs)
   Serial.print("Speed (kph): ");
   Serial.print(gps.speed.kmph());
