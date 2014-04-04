26938cabff6fa8124847b82658acd908e89617e3
Ben Lieber <blieber@comverge.com>
2014-04-03 22:19:07 -0600

    add drive and steer methods
    to control motor and servo based on range sensor

--- ProjectPbr.ino
+++ ProjectPbr.ino
@@ -2,22 +2,45 @@
 #include "Adafruit_GPS.h"
 #include "SoftwareSerial.h"
 #include "TinyGPS++.h"
+#include <Servo.h>
 
 // pins:
-int const GREEN_LEFT   = 2,  RED_LEFT     = 3,  YELLOW_LEFT = 4;
-int const GREEN_RIGHT  = 8,  RED_RIGHT    = 9,  YELLOW_RIGHT = 10;
-int const RANGE_LEFT   = 7,  RANGE_RIGHT  = 6;
-
-// configuration:
-int const STOP_DIST_CM = 25, SLOW_DIST_CM = 50, READ_FREQ_MS = 300;
-
+int const GREEN_LEFT   = 2,  RED_LEFT      = 3,  YELLOW_LEFT  = 4;
+int const GREEN_RIGHT  = 8,  RED_RIGHT     = 9,  YELLOW_RIGHT = 10;
+int const RANGE_LEFT   = 7,  RANGE_RIGHT   = 6;
+int const MOTOR_FORWARD = 22,  MOTOR_REVERSE = 24, DUTY_CYCLE  = 13;
+int const SERVO = 11;
 // Not configurable:
 // GPS_RX = 18, GPS_TX = 19
+
+// configuration:
+int const STOP_DIST_CM = 25, SLOW_DIST_CM = 50;
+int const READ_FREQ_MS = 300;
+int const WAY_POINT_RADIUS_CM = 100;
+int const FORWARD = 1, STOP = 0, REVERSE = -1;
+int const STRAIGHT = 90, SOFT_LEFT = 105, SOFT_RIGHT = 75, HARD_LEFT = 125, HARD_RIGHT = 55;
+
+float ROT;
+float DUTY;
+
+float wayPoints[7][2] = {
+ {-105.154025, 39.924652},
+ {-105.154013, 39.924672},
+ {-105.153957, 39.924659},
+ {-105.153916, 39.924646},
+ {-105.153897, 39.924604},
+ {-105.153957, 39.924618},
+ {-105.154013, 39.924629}
+};
+
+int curWayPoint = 0;
 
 Adafruit_GPS GPS(&Serial1);
 // #define GPSECHO  false // debug?
 
 TinyGPSPlus gps;
+Servo myservo;
+
 
 class Ultrasonic {
 	public:
@@ -40,9 +63,9 @@ void Ultrasonic::DistanceMeasure(void) {
 	delayMicroseconds(2);
 	digitalWrite(_pin, HIGH);
 	delayMicroseconds(5);
-	digitalWrite(_pin,LOW);
-	pinMode(_pin,INPUT);
-	duration = pulseIn(_pin,HIGH);
+	digitalWrite(_pin, LOW);
+	pinMode(_pin, INPUT);
+	duration = pulseIn(_pin, HIGH);
 }
 
 // The measured distance from the range 0 to 400 Centimeters
@@ -62,9 +85,16 @@ void setup() {
   pinMode(RED_RIGHT, OUTPUT);
   pinMode(YELLOW_RIGHT, OUTPUT);
 
+  pinMode(MOTOR_FOWARD, OUTPUT);
+  pinMode(MOTOR_REVERSE, OUTPUT);
+  pinMode(DUTY_CYCLE, OUTPUT);
+
+  myservo.attach(SERVO);
+
 	Serial.begin(115200);
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // Set the update rate: 1 Hz
+  GPS.sendCommand("$PMTK301,1*2D");
   useInterrupt();
   delay(1000);
 }
@@ -84,9 +114,7 @@ uint32_t timer = millis();
 
 void loop() {
 	if (gps.location.isUpdated()) {
-    Serial.print("Passed Checksum:");
-    Serial.print(!gps.failedChecksum());
-    Serial.print("; ");
+    // Serial.println(GPS.lastNMEA());
 		printGpsInfo();
     ultrasonic_right.DistanceMeasure(); // get the current signal time;
     ultrasonic_left.DistanceMeasure(); // get the current signal time;
@@ -102,6 +130,30 @@ void updateSpeedIndicators(int pin1, int pin2) {
   analogWrite(pin2, 50);
 }
 
+void drive(int dir, int speed) {
+  float duty = (speed / 100.00) * 255;
+  boolean forward, reverse;
+
+  if(dir == 1)  {
+    forward = HIGH;
+    reverse = LOW;
+  } else if(dir == -1) {
+    forward = LOW;
+    reverse = HIGH;
+  } else if(dir = 0) {
+    forward = LOW;
+    reverse = LOW;
+  }
+
+  digitalWrite(MOTOR_FORWARD, forward);
+  digitalWrite(MOTOR_REVERSE, reverse);
+  analogWrite(DUTY_CYCLE, duty);
+}
+
+void steer(int pos_deg) {
+  myservo.write(pos_deg);
+}
+
 void lightsOut() {
   analogWrite(GREEN_LEFT, 0);
   analogWrite(YELLOW_LEFT, 0);
@@ -112,53 +164,113 @@ void lightsOut() {
 }
 
 void printRangeInfo(long rangeCmL, long rangeCmR) {
-	Serial.print(rangeCmL);
-	Serial.print("cm, ");
+	Serial.print("Dist L/R (cm): ");
+  Serial.print(rangeCmL);
+	Serial.print("/");
+  Serial.print(rangeCmL);
+  Serial.print("; ");
 	if (rangeCmL < STOP_DIST_CM && rangeCmR < STOP_DIST_CM) {
     updateSpeedIndicators(RED_LEFT, RED_RIGHT);
-    Serial.print("Reverse!");
+    Serial.print("R");
+    drive(STOP, 0);
+    steer(STRAIGHT);
   } else if (rangeCmR < STOP_DIST_CM && rangeCmL > STOP_DIST_CM) {
     updateSpeedIndicators(GREEN_LEFT, RED_RIGHT);
-    Serial.print("Hard left");
+    Serial.print("HL");
+    drive(FORWARD, 20);
+    steer(HARD_LEFT);
   } else if (rangeCmL < STOP_DIST_CM && rangeCmR > STOP_DIST_CM) {
 		updateSpeedIndicators(RED_LEFT, GREEN_RIGHT);
-		Serial.print("Hard right");
+		Serial.print("HR");
+    drive(FORWARD, 20);
+    steer(HARD_RIGHT);
   } else if (rangeCmL < SLOW_DIST_CM && rangeCmR < SLOW_DIST_CM) {
     updateSpeedIndicators(YELLOW_LEFT, YELLOW_RIGHT);
-    Serial.print("Slow down");
+    Serial.print("S");
+    drive(REVERSE, 40);
+    steer(STRAIGHT);
   } else if (rangeCmR < SLOW_DIST_CM && rangeCmL > SLOW_DIST_CM) {
     updateSpeedIndicators(GREEN_LEFT, YELLOW_RIGHT);
-    Serial.print("Soft left");
+    Serial.print("EL");
+    drive(REVERSE, 40);
+    steer(SOFT_LEFT);
   } else if (rangeCmL < SLOW_DIST_CM && rangeCmR > SLOW_DIST_CM) {
     updateSpeedIndicators(YELLOW_LEFT, GREEN_RIGHT);
-    Serial.print("Soft right");
+    Serial.print("ER");
+    drive(REVERSE, 40);
+    steer(SOFT_RIGHT);
 	} else {
 		updateSpeedIndicators(GREEN_LEFT, GREEN_RIGHT);
-		Serial.print("BALLS TO THE WALL!");
+		Serial.print("G");
+    drive(FORWARD, 60);
+    steer(STRAIGHT);
 	}
 }
 
 void printGpsInfo() {
+  // static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+  // double distanceToLondon = TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), LONDON_LAT, LONDON_LON);
+  // double courseToLondon = TinyGPSPlus::courseTo(gps.location.lat(), gps.location.lng(), LONDON_LAT, LONDON_LON);
+  Serial.print("Checksum:");
+  if (gps.failedChecksum()) {
+    Serial.print("ok");
+  } else {
+    Serial.print("fail");
+  }
   Serial.print("; ");
-  Serial.print("Position: ");
-  Serial.print(gps.location.lat());
-  // Serial.println(gps.location.rawLatDegrees()); // Raw latitude in whole degrees (no work!)
-  // Serial.println(gps.location.rawLatBillionths()); // ... and billionths (i16/u32) (no work!)
+
+  Serial.print("Next WP: #");
+  Serial.print(curWayPoint);
+  Serial.print(": ");
+  Serial.print(wayPoints[curWayPoint][0], 6);
   Serial.print(", ");
-  Serial.print(gps.location.lng());
-  // Serial.println(gps.location.rawLngDegrees()); // Raw longitude in whole degrees (no work!)
-  // Serial.println(gps.location.rawLngBillionths()); // ... and billionths (i16/u32) (no work!)
-  Serial.print("(within ");
-  Serial.print(gps.hdop.value());
-  Serial.print("); ");
-  // Serial.println(gps.time.value()); // Raw time in HHMMSSCC format (u32) (keeping for last read calcs)
-  Serial.print("Speed (kph): ");
-  Serial.print(gps.speed.kmph());
+  Serial.print(wayPoints[curWayPoint][1], 6);
   Serial.print("; ");
+
+  Serial.print("Dist (m):");
+  Serial.print(distToWayPoint());
+  Serial.print("; ");
+
+  Serial.print("Course (deg):");
+  Serial.print(courseToWayPoint());
+  Serial.print("; ");
+
   Serial.print("Heading (deg): ");
   Serial.print(gps.course.deg());
   Serial.print("; ");
-  Serial.print("Altitude: ");
-  Serial.print(gps.altitude.kilometers());
+
+  Serial.print("Position: ");
+  Serial.print(gps.location.lat(), 6);
+  Serial.print(", ");
+  Serial.print(gps.location.lng(), 6);
   Serial.print("; ");
+
+  Serial.print("Speed (kph): ");
+  Serial.print(gps.speed.kmph());
+  Serial.print("; ");
+}
+
+boolean achievementUnlocked() {
+  if(distToWayPoint() < WAY_POINT_RADIUS_CM) {
+    curWayPoint += 1;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+long distToWayPoint() {
+  return TinyGPSPlus::distanceBetween(
+    gps.location.lat(),
+    gps.location.lng(),
+    wayPoints[curWayPoint][1],
+    wayPoints[curWayPoint][0]);
+}
+
+long courseToWayPoint() {
+  return TinyGPSPlus::courseTo(
+    gps.location.lat(),
+    gps.location.lng(),
+    wayPoints[curWayPoint][1],
+    wayPoints[curWayPoint][0]);
 }
